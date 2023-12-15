66f5d10e8708b4938a0919ce2c634503e9a59b5e
UltraGnome <ultragnomeultra@duck.com>
2023-12-14 20:16:30 -0600

    ready to chase the critter in day 10

--- day_09/solution.py
+++ day_09/solution.py
@@ -1,29 +1,47 @@
 class Part1:
     @staticmethod
     def solution(file_lines: list[str]) -> int:
-        """
+        result = 0
+        next_numbers_for_all_sequences = []
 
-        Args:
-            file_lines: List of lines from input file
+        for line in file_lines:
+            line_array = line.split(' ')
+            sequence = [int(x) for x in line_array]
+            next_numbers_for_all_sequences.append(Part1.predict_next_number(sequence))
 
-        Returns:
+        result = sum(next_numbers_for_all_sequences)
 
-        """
-        return 0
+
+        return result
+
+    @staticmethod
+    def predict_next_number(seq, prev=0):
+        if seq:
+            if len(seq) == 1:
+                return prev + seq[-1] # Predict next in sequence as last value + difference
+
+            return Part1.predict_next_number([b - a for a, b in zip(seq[:-1], seq[1:])], prev + seq[-1]) # Find prediction for next difference
+        else:
+            return 0
 
 
 class Part2:
     @staticmethod
     def solution(file_lines: list[str]) -> int:
-        """
+        result = 0
+        previous_numbers_for_all_sequences = []
 
-        Args:
-            file_lines: List of lines from input file
+        for line in file_lines:
 
-        Returns:
+            line_array = line.split(' ')
+            sequence = [int(x) for x in line_array]
+            sequence.reverse()
+            previous_numbers_for_all_sequences.append(Part1.predict_next_number(sequence))
 
-        """
-        return 0
+        result = sum(previous_numbers_for_all_sequences)
+
+
+        return result
 
 
 with open("input.txt", "r") as file:
