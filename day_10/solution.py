836bf026e19415930d6c50ec3827edc111d9022b
UltraGnome <ultragnomeultra@duck.com>
2023-12-16 18:44:44 -0600

    grids

--- day_10/solution.py
+++ day_10/solution.py
@@ -1,29 +1,101 @@
 class Part1:
     @staticmethod
     def solution(file_lines: list[str]) -> int:
-        """
+        # Find S
+        # Find the entire loop
+        #     left of S
+        #         find corner
+        #     right of S
+        #         find corner
+        # loop steps halved is the distance to furthest point
+        graph = {}
+        for x, line in enumerate(file_lines): #iterate over the lines
+            for y, char in enumerate(line.upper()): # search for the start
+                adjacent = []
+                if char in '-J7S':
+                    adjacent.append((x, y-1))
+                if char in '-FLS':
+                    adjacent.append((x, y+1))
+                if char in '|F7S':
+                    adjacent.append((x+1, y))
+                if char in '|LJS':
+                    adjacent.append((x-1, y))
+                if char == 'S':
+                    visited = set([(x, y)])
+                    q = set([(x, y)])
+                graph[(x, y)] = adjacent
 
-        Args:
-            file_lines: List of lines from input file
+        steps = -1
+        while q:
+            nxt = set()
+            for x1, y1 in q:
+                for x2, y2 in graph[(x1, y1)]:
+                    if (x2, y2) not in visited and (x1, y1) in graph.get((x2, y2), []):
+                        nxt.add((x2, y2))
+                        visited.add((x2, y2))
+            q = nxt
+            steps += 1
 
-        Returns:
-
-        """
-        return 0
-
+        return steps
 
 class Part2:
     @staticmethod
     def solution(file_lines: list[str]) -> int:
-        """
 
-        Args:
-            file_lines: List of lines from input file
+        graph = {}
+        for x, line in enumerate(file_lines):
+            for y, tile in enumerate(line):
+                adjacent = []
+                if tile in '-J7S':
+                    adjacent.append((x, y-1))
+                if tile in '-FLS':
+                    adjacent.append((x, y+1))
+                if tile in '|F7S':
+                    adjacent.append((x+1, y))
+                if tile in '|LJS':
+                    adjacent.append((x-1, y))
+                if tile == 'S':
+                    tile_q = set([(x, y)])
+                graph[(x, y)] = adjacent
 
-        Returns:
+        pipes = set()
+        while tile_q:
+            nxt = set()
+            for x1, y1 in tile_q:
+                for x2, y2 in graph[(x1, y1)]:
+                    if (x1, y1) not in graph.get((x2, y2), []):
+                        continue
+                    pipe = (*sorted((x1, x2)), *sorted((y1, y2)))
+                    if pipe not in pipes:
+                        pipes.add(pipe)
+                        nxt.add((x2, y2))
+            tile_q = nxt
 
-        """
-        return 0
+        m, n = len(file_lines), len(file_lines[0])
+        visited = set()
+        corner_q = [(0, 0)]
+
+        while corner_q:
+            x, y = corner_q.pop()
+            requirements = (x > 0, y < n, x < m, y > 0)
+            adjacent = ((x-1, y), (x, y+1), (x+1, y), (x, y-1))
+            tile_pairs = ((x-1, x-1, y-1, y),   # up
+                          (x-1, x, y, y),       # right
+                          (x, x, y-1, y),       # down
+                          (x-1, x, y-1, y-1))   # left
+            for req, corner, tile_pair in zip(requirements, adjacent, tile_pairs):
+                if req and corner not in visited and tile_pair not in pipes:
+                    visited.add(corner)
+                    corner_q.append(corner)
+
+        total = m * n - len(pipes)
+        for i in range(m):
+            for j in range(n):
+                corners = ((i, j), (i+1, j), (i, j+1), (i+1, j+1))
+                if all(c in visited for c in corners):
+                    total -= 1
+
+        return total
 
 
 with open("input.txt", "r") as file:
--- day_11/solution.py
+++ day_11/solution.py
@@ -1,35 +1,52 @@
 class Part1:
     @staticmethod
     def solution(file_lines: list[str]) -> int:
-        """
+        result = 0
+        galaxy_map = [list(line) for line in file_lines]
 
-        Args:
-            file_lines: List of lines from input file
+        empty_rows = [i for i, row in enumerate(
+            galaxy_map) if all(char == '.' for char in row)]
 
-        Returns:
+        empty_cols = [i for i, col in enumerate(zip(*galaxy_map)) if all(
+            char == '.' for char in col)]
 
-        """
-        return 0
+        galaxies = [(row, col) for row, line in enumerate(galaxy_map)
+                    for col, char in enumerate(line) if char == '#']
 
+        for i, (galaxy_row, galaxy_col) in enumerate(galaxies):
+            for (other_galaxy_row, other_galaxy_col) in galaxies[:i]:
+                for row in range(min(other_galaxy_row, galaxy_row), max(other_galaxy_row, galaxy_row)):
+                    result += 2 if row in empty_rows else 1
+
+                for col in range(min(other_galaxy_col, galaxy_col), max(other_galaxy_col, galaxy_col)):
+                    result += 2 if col in empty_cols else 1
+        return result
 
 class Part2:
     @staticmethod
     def solution(file_lines: list[str]) -> int:
-        """
+        result = 0
+        empty_rows = [r for r, row in enumerate(file_lines) if all(ch == "." for ch in row)]
+        empty_cols = [c for c, col in enumerate(zip(*file_lines)) if all(ch == "." for ch in col)]
 
-        Args:
-            file_lines: List of lines from input file
+        points = [(r, c) for r, row in enumerate(file_lines) for c, ch in enumerate(row) if ch == "#"]
 
-        Returns:
 
-        """
-        return 0
+        scale = 1000000
+
+        for i, (r1, c1) in enumerate(points):
+            for (r2, c2) in points[:i]:
+                for r in range(min(r1, r2), max(r1, r2)):
+                    result += scale if r in empty_rows else 1
+                for c in range(min(c1, c2), max(c1, c2)):
+                    result += scale if c in empty_cols else 1
+        return result
 
 
 with open("input.txt", "r") as file:
     f = file.read().splitlines()
 
-print(f"Part 1: {Part1.solution(f)}")
+# print(f"Part 1: {Part1.solution(f)}")
 print(f"Part 2: {Part2.solution(f)}")
 
 
