5051b45cb157598a5d002f764558538fc9e9dbb2
UltraGnome <ultragnomeultra@duck.com>
2023-12-17 18:16:17 -0600

    days 12 and 13

--- day_12/solution.py
+++ day_12/solution.py
@@ -1,35 +1,81 @@
 class Part1:
     @staticmethod
     def solution(file_lines: list[str]) -> int:
-        """
 
-        Args:
-            file_lines: List of lines from input file
+        result = 0
 
-        Returns:
+        for line in file_lines:
+            spring_conditions, spring_arrangement = line.split()
+            spring_arrangement = tuple(map(int, spring_arrangement.split(",")))
+            result += Part1.count_possible_arrangements(spring_conditions, spring_arrangement)
 
-        """
-        return 0
+        return result
 
-
+    @staticmethod
+    def count_possible_arrangements(spring_conditions, spring_arrangement):
+        if spring_conditions == "":
+            return 1 if spring_arrangement == () else 0
+    
+        if spring_arrangement == ():
+            return 0 if "#" in spring_conditions else 1
+    
+        result = 0
+    
+        if spring_conditions[0] in ".?":
+            result += Part1.count_possible_arrangements(spring_conditions[1:], spring_arrangement)
+    
+        if spring_conditions[0] in "#?":
+            if spring_arrangement[0] <= len(spring_conditions) and "." not in spring_conditions[:spring_arrangement[0]] and (spring_arrangement[0] == len(spring_conditions) or spring_conditions[spring_arrangement[0]] != "#"):
+                result += Part1.count_possible_arrangements(spring_conditions[spring_arrangement[0] + 1:], spring_arrangement[1:])
+    
+        return result
 class Part2:
     @staticmethod
     def solution(file_lines: list[str]) -> int:
-        """
+        cache = {}
 
-        Args:
-            file_lines: List of lines from input file
+        def count(cfg, nums):
+            if cfg == "":
+                return 1 if nums == () else 0
 
-        Returns:
+            if nums == ():
+                return 0 if "#" in cfg else 1
 
-        """
-        return 0
+            key = (cfg, nums)
+
+            if key in cache:
+                return cache[key]
+
+            result = 0
+
+            if cfg[0] in ".?":
+                result += count(cfg[1:], nums)
+
+            if cfg[0] in "#?":
+                if nums[0] <= len(cfg) and "." not in cfg[:nums[0]] and (nums[0] == len(cfg) or cfg[nums[0]] != "#"):
+                    result += count(cfg[nums[0] + 1:], nums[1:])
+
+            cache[key] = result
+            return result
+
+        total = 0
+
+        for line in file_lines:
+            cfg, nums = line.split()
+            nums = tuple(map(int, nums.split(",")))
+
+            cfg = "?".join([cfg] * 5)
+            nums *= 5
+
+            total += count(cfg, nums)
+
+        return total
 
 
 with open("input.txt", "r") as file:
     f = file.read().splitlines()
 
-print(f"Part 1: {Part1.solution(f)}")
+# print(f"Part 1: {Part1.solution(f)}")
 print(f"Part 2: {Part2.solution(f)}")
 
 
--- day_13/solution.py
+++ day_13/solution.py
@@ -1,15 +1,46 @@
 class Part1:
     @staticmethod
-    def solution(file_lines: list[str]) -> int:
+    def solution(file_contents) -> int:
         """
+            get rows
+            get cols
+            traverse, comparing n to n+1
 
-        Args:
-            file_lines: List of lines from input file
-
-        Returns:
 
         """
-        return 0
+        valley_of_mirrors_patterns = file_contents.split('\n\n')
+
+        result = 0
+        for pattern in valley_of_mirrors_patterns:
+            valley_of_mirrors_map: object = pattern.splitlines()
+            rows = []
+            cols = []
+            for row in valley_of_mirrors_map:
+                rows.append(row)
+
+            result += (Part1.find_reflection_padding(rows) * 100)
+
+            for column in list(zip(*valley_of_mirrors_map)):
+                cols.append(''.join(column))
+
+            result += (Part1.find_reflection_padding(cols))
+
+        return result
+
+    @staticmethod
+    def find_reflection_padding(rows):
+        result = 0
+        for r in range(1, len(rows)):
+            line1 = rows[:r][::-1]
+            line2 = rows[r:]
+
+            line1 = line1[:len(line2)]
+            line2 = line2[:len(line1)]
+
+            if line1 == line2:
+                result = r
+                return result
+        return result
 
 
 class Part2:
@@ -27,10 +58,7 @@ class Part2:
 
 
 with open("input.txt", "r") as file:
-    f = file.read().splitlines()
+    f = file.read()
 
 print(f"Part 1: {Part1.solution(f)}")
-print(f"Part 2: {Part2.solution(f)}")
-
-
-
+# print(f"Part 2: {Part2.solution(f)}")
