3690a996c83f5c64afbd15ab3144f676ea4c52ca
UltraGnome <ultragnomeultra@duck.com>
2023-12-20 19:27:36 -0600

    days 12 and 13

--- day_13/solution.py
+++ day_13/solution.py
@@ -31,7 +31,7 @@ class Part1:
     def find_reflection_padding(rows):
         result = 0
         for r in range(1, len(rows)):
-            line1 = rows[:r][::-1]
+            line1 = rows[:r][::-1]  #::-1 means start at the end
             line2 = rows[r:]
 
             line1 = line1[:len(line2)]
@@ -44,21 +44,44 @@ class Part1:
 
 
 class Part2:
-    @staticmethod
-    def solution(file_lines: list[str]) -> int:
-        """
 
-        Args:
-            file_lines: List of lines from input file
-
-        Returns:
-
-        """
+    def find_smudge_padding(grid):
+        for i in range(1, len(grid)):
+            line1 = grid[:i][::-1]
+            line2 = grid[i:]
+    
+            total_mismatches = 0
+            for top_row, bottom_row in zip(line1, line2):
+                for (top_char, bottom_char) in zip(top_row, bottom_row):
+                    if top_char != bottom_char:
+                        total_mismatches += 1
+            # we only want the pairs with one mismatch
+            if total_mismatches == 1:
+                return i
+    
         return 0
+    
+    
+    @staticmethod
+    def solution(file_contents) -> int:
+        valley_of_mirrors_patterns = file_contents.split('\n\n')
+
+        result = 0
+        for pattern in valley_of_mirrors_patterns:
+            grid = pattern.splitlines()
+
+            row_index = Part2.find_smudge_padding(grid)
+            result += row_index * 100
+
+            transposed_grid = list(zip(*grid))
+            column_index = Part2.find_smudge_padding(transposed_grid)
+            result += column_index
+
+        return result
 
 
 with open("input.txt", "r") as file:
     f = file.read()
 
-print(f"Part 1: {Part1.solution(f)}")
-# print(f"Part 2: {Part2.solution(f)}")
+# print(f"Part 1: {Part1.solution(f)}")
+print(f"Part 2: {Part2.solution(f)}")
