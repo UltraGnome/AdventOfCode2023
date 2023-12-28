8c573c2551e6a30eea0ec9d60ce390f3f7f44688
UltraGnome <ultragnomeultra@duck.com>
2023-12-28 16:36:51 -0600

    days 15

--- day_15/solution.py
+++ day_15/solution.py
@@ -1,33 +1,60 @@
+import re
+from functools import reduce
+from collections import defaultdict
+
 class Part1:
     @staticmethod
-    def solution(file_lines: list[str]) -> int:
-        """
+    def solution(instructions) -> int:
+        result = 0
 
-        Args:
-            file_lines: List of lines from input file
+        for step in instructions:
+            step_score = Part1.score_step(step)
+            result += step_score
 
-        Returns:
+        return result
 
-        """
-        return 0
+    @staticmethod
+    def score_step(step):
+        step_score = 0
+        for letter in list(step):
+            step_score += ord(letter)
+            step_score = step_score * 17
+            step_score = step_score % 256
+        return step_score
 
 
 class Part2:
     @staticmethod
-    def solution(file_lines: list[str]) -> int:
-        """
+    def solution(instructions) -> int:
+        boxes = [dict() for x in range(256)]
 
-        Args:
-            file_lines: List of lines from input file
+        for i, s in enumerate(instructions):
+            if "-" in s:
+                lens_id = s[:s.index("-")]
+                box_id = Part1.score_step(lens_id)
 
-        Returns:
+                if lens_id in boxes[box_id]:
+                    del boxes[box_id][lens_id]
+            elif "=" in s:
+                lens_id = s[:s.index("=")]
+                box_id = Part1.score_step(lens_id)
+                lens_focal = int(s[s.index("=")+1:])
+                boxes[box_id][lens_id] = lens_focal
 
-        """
-        return 0
+            else:
+                print("ERROR", s)
+
+        result = 0
+        for box_id, box in enumerate(boxes):
+            for slot_id, lens in enumerate(box):
+                result += (box_id+1) * (slot_id+1) * box[lens]
+
+        return result
+
 
 
 with open("input.txt", "r") as file:
-    f = file.read().splitlines()
+    f = file.read().split(',')
 
 print(f"Part 1: {Part1.solution(f)}")
 print(f"Part 2: {Part2.solution(f)}")
