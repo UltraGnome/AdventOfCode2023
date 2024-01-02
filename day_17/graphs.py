b5c001422a175b2fe43138ce974f0cd71e0944a2
UltraGnome <ultragnomeultra@duck.com>
2024-01-02 09:29:27 -0600

    days 17

---
+++ day_17/graphs.py
@@ -0,0 +1,116 @@
+from enum import IntEnum
+from itertools import product
+from operator import itemgetter
+from typing import Iterator, Literal
+
+GridPoint = tuple[int, int]
+Grid = dict[GridPoint, str]
+
+DIRECTIONS = sorted(product((-1, 0, 1), repeat=2), key=itemgetter(1))
+
+
+def neighbors(
+        center: GridPoint,
+        num_directions=8,
+        *,
+        ignore_negatives: bool = False,
+        max_size: int = 0,
+        max_x_size: int = 0,
+        max_y_size: int = 0,
+) -> Iterator[tuple[int, int]]:
+    """
+    given a point (2-tuple) it yields each neighboring point.
+    Iterates from top left to bottom right, skipping any points as described below:
+
+    * `num_directions`: Can get cardinal directions (4), include diagonals (8), or include self (9)
+    * `ignore_negatives`: skips points where either value is less than 0
+    * `max_DIM_size`: if specified, skips points where the dimension value is greater than the max grid size in that dimension. If doing a 2D-List based (aka `(row,col)` grid) rather than a pure `(x,y)` grid, the max values should be `len(DIM) - 1`. Is mutually exclusive with `max_size`.
+
+    For a 2D list-based grid, neighbors will come out in (row, col) format.
+    """
+    assert num_directions in {4, 8, 9}
+    # one or the other
+    if max_size:
+        assert not (max_x_size or max_y_size)
+    if max_x_size or max_y_size:
+        assert not max_size
+
+    for dx, dy in DIRECTIONS:
+        if num_directions == 4 and dx and dy:
+            # diagonal; skip
+            continue
+
+        if num_directions != 9 and not (dx or dy):
+            # skip self
+            continue
+
+        rx = center[0] + dx
+        ry = center[1] + dy
+
+        if ignore_negatives and (rx < 0 or ry < 0):
+            continue
+
+        if max_size and (rx > max_size or ry > max_size):
+            continue
+
+        if max_x_size and (rx > max_x_size):
+            continue
+
+        if max_y_size and (ry > max_y_size):
+            continue
+
+        yield (rx, ry)
+
+
+def parse_grid(raw_grid: list[str], ignore_chars: str = "") -> Grid:
+    """
+    returns 2-tuples of (row, col) with their value
+    """
+    result = {}
+    ignore = set(ignore_chars)
+
+    for row, line in enumerate(raw_grid):
+        for col, c in enumerate(line):
+            if c in ignore:
+                continue
+            result[row, col] = c
+
+    return result
+
+
+def add_points(a: GridPoint, b: GridPoint) -> GridPoint:
+    """
+    add a pair of 2-tuples together. Useful for calculating a new position from a location and an offset
+    """
+    return a[0] + b[0], a[1] + b[1]
+
+
+def taxicab_distance(a: GridPoint, b: GridPoint) -> int:
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
+Rotation = Literal["CCW", "CW"]
+
+
+class Direction(IntEnum):
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+
+    @staticmethod
+    def rotate(facing: "Direction", towards: Rotation) -> "Direction":
+        offset = 1 if towards == "CW" else -1
+        return Direction((facing.value + offset) % 4)
+
+    @staticmethod
+    def offset(facing: "Direction") -> GridPoint:
+        return _ROW_COLL_OFFSETS[facing]
+
+
+_ROW_COLL_OFFSETS: dict[Direction, GridPoint] = {
+    Direction.UP: (-1, 0),
+    Direction.RIGHT: (0, 1),
+    Direction.DOWN: (1, 0),
+    Direction.LEFT: (0, -1),
+}
--- day_17/solution.py
+++ day_17/solution.py
@@ -1,36 +1,139 @@
+from heapq import heappop, heappush
+from typing import NamedTuple
+from graphs import Direction, GridPoint, Rotation, add_points, parse_grid
+
+class Position(NamedTuple):
+    loc: GridPoint
+    facing: Direction
+
+    @property
+    def next_loc(self) -> GridPoint:
+        return add_points(self.loc, Direction.offset(self.facing))
+
+    def step(self) -> "Position":
+        return Position(self.next_loc, self.facing)
+
+    def rotate_and_step(self, towards: Rotation):
+        return Position(self.loc, Direction.rotate(self.facing, towards)).step()
+
+
+# cost, position, number of steps in the same direction (max 3)
+State = tuple[int, Position, int]
 class Part1:
+
     @staticmethod
-    def solution(file_lines: list[str]) -> int:
-        """
+    def solution(file_lines: list[str], min_steps:int, max_steps:int) -> int:
+        result = -1
+        target = len(file_lines) - 1, len(file_lines[-1]) - 1
+        grid = {k: int(v) for k, v in parse_grid(file_lines).items()}
 
-        Args:
-            file_lines: List of lines from input file
+        queue: list[State] = [
+            (0, Position((0, 0), Direction.DOWN), 0),
+            (0, Position((0, 0), Direction.RIGHT), 0),
+        ]
+        seen: set[tuple[Position, int]] = set()
 
-        Returns:
+        while queue:
+            cost, pos, num_steps = heappop(queue)
 
-        """
-        return 0
+            if pos.loc == target and num_steps >= min_steps:
+                result = cost
+                return result
 
+            if (pos, num_steps) in seen:
+                continue
+            seen.add((pos, num_steps))
+
+            if (
+                    num_steps >= min_steps
+                    and (left := pos.rotate_and_step("CCW")).loc in grid
+            ):
+                heappush(queue, (cost + grid[left.loc], left, 1))
+
+            if (
+                    num_steps >= min_steps
+                    and (right := pos.rotate_and_step("CW")).loc in grid
+            ):
+                heappush(queue, (cost + grid[right.loc], right, 1))
+
+            if num_steps < max_steps and (forward := pos.step()).loc in grid:
+                heappush(queue, (cost + grid[forward.loc], forward, num_steps + 1))
+
+        return result
+
+    def dijkstra(file_lines: list[str]) -> int:
+        result = -1
+        target = len(file_lines) - 1, len(file_lines[-1]) - 1
+        grid = {k: int(v) for k, v in parse_grid(file_lines).items()}
+
+        queue: list[State] = [
+            (0, Position((0, 0), Direction.DOWN), 0),
+            (0, Position((0, 0), Direction.RIGHT), 0),
+        ]
+        seen: set[tuple[Position, int]] = set()
+
+        while queue:
+            cost, pos, num_steps = heappop(queue)
+
+            if pos.loc == target:
+                result = cost
+                return result
+
+            if (pos, num_steps) in seen:
+                continue
+            seen.add((pos, num_steps))
+
+            if (
+                    left := pos.rotate_and_step("CCW").loc in grid
+            ):
+                heappush(queue, (cost + grid[left.loc], left, 1))
+
+            if (
+                    right := pos.rotate_and_step("CW").loc in grid
+            ):
+                heappush(queue, (cost + grid[right.loc], right, 1))
+
+            if forward := pos.step().loc in grid:
+                heappush(queue, (cost + grid[forward.loc], forward, num_steps + 1))
+
+        return result
 
 class Part2:
     @staticmethod
     def solution(file_lines: list[str]) -> int:
-        """
+        Part1.solution(file_lines,4,10)
 
-        Args:
-            file_lines: List of lines from input file
 
-        Returns:
-
-        """
         return 0
 
 
 with open("input.txt", "r") as file:
     f = file.read().splitlines()
-
-print(f"Part 1: {Part1.solution(f)}")
-print(f"Part 2: {Part2.solution(f)}")
+    # just Dijsktra
 
 
+print(f"just Dijsktra: {Part1.dijkstra(f)}")
+print(f"Part 1: {Part1.solution(f,0,3)}")
+print(f"Part 2: {Part1.solution(f, 4, 10)}")
 
+
+
+
+
+
+
+#
+# class Solution(StrSplitSolution):
+#     _year = 2023
+#     _day = 17
+#
+#     def _solve(self, min_steps: int, max_steps: int) -> int:
+#
+#     @answer(1244)
+#     def part_1(self) -> int:
+#         return self._solve(0, 3)
+#
+#     @slow
+#     @answer(1367)
+#     def part_2(self) -> int:
+#         return self._solve(4, 10)
